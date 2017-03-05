66eaa8e60435c0aa8e3d87716423d6b5e9da2e0a
yintothayang <yintothayang@gmail.com>
2017-03-04 17:57:39 -0800

    export components

--- src/main.js
+++ src/main.js
@@ -5,3 +5,11 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+
+// Export components 
+export {default as Button} from './components/button.vue'
+export {default as Input} from './components/input.vue'
+export {default as TextArea} from './components/textarea.vue'
+export {default as Select} from './components/select.vue'
